38321de9b4cb75a40449c3590369dcd38a90bf11
Timur <User@LAPTOP-Q3NBDU5U>
2023-12-31 00:40:40 +0400

    Add project files.

---
+++ PropertysExperience/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public sealed class AipEmployee // AIP(Automatically Implemented Properties) - автосвойства крч
+{
+    public string Name { get; set; }
+    private int m_Age;
+    public int Age
+    {
+        get { return m_Age; }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("value", value.ToString(), "The value must be greater or equal to 0");
+                m_Age = value;
+            }
+        }
+    }
+}
+
+// Ну, согласно Рихтеру, автосвойства - полная дичь, которой пользуются зря, и вообще удалить их надо из мира
+// методы лучше, там возможножностей больше и т.д., ну мне впринципе все равно, мне и так, и по другому писать приятно
---
+++ PropertysExperience/SimpleEmployee.cs
@@ -0,0 +1,27 @@
+﻿public sealed class SimpleEmployee
+{
+    private string? m_Name;
+    private int m_Age;
+
+    public string get_Name()
+    {
+        return m_Name;
+    }
+
+    public void set_Name(string name)
+    {
+        m_Name = name;
+    }
+
+    public int get_Age()
+    {
+        return m_Age;
+    }
+
+    public void set_Age(int age)
+    {
+        if (age < 0)
+            throw new ArgumentOutOfRangeException("age", age.ToString(), "The age must be greater or equal to 0");
+        m_Age = age;
+    }
+}
